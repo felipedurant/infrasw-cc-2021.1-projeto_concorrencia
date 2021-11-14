9a654224dcc832b1fc589977f9f8ab72017e9adf
carlosrcp <crcp@cin.ufpe.br>
2021-11-14 10:23:25 -0300

    abrindo a janela do player

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import ui.PlayerWindow;
+
 public class Main {
     public static void main(String[] args) {
+        Thread t = new PlayerWindow(null, null, null,
+                null, null, null, null,
+                null, null, null, null,
+                "Nome janela", null);
+
+        t.start();
     }
 }
