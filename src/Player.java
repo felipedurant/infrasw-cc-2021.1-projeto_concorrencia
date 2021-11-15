3613bcc50b2869022dfffc399de576248999b5f8
Carlos <crcp@cin.ufpe.br>
2021-11-15 20:14:34 -0300

    janelas abrindo

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,30 @@
+import ui.AddSongWindow;
+import ui.PlayerWindow;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
 public class Player {
+
+    AddSongWindow addSongWindow;
+    PlayerWindow window;
+
     public Player() {
+
+        ActionListener btnAddSongOK = e ->{
+
+        };
+
+        ActionListener btnAddSong = e ->{
+            addSongWindow = new AddSongWindow(null, btnAddSongOK, window.getAddSongWindowListener());
+        };
+
+        window = new PlayerWindow(null, null, btnAddSong,
+                null, null, null, null,
+                null, null, null, null,
+                "Nome janela", null);
+
+        window.start();
     }
 }
 
