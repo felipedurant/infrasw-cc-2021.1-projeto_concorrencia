1712c1b278b6f71f3239a732c198a70dd97954d2
Carlos <crcp@cin.ufpe.br>
2021-11-15 21:10:15 -0300

    adicionando musicas a janela principal

--- src/Player.java
+++ src/Player.java
@@ -9,14 +9,36 @@ public class Player {
     AddSongWindow addSongWindow;
     PlayerWindow window;
 
+    // contador que serve para o songID
+    int songID=0;
+
+    // contador de quantas musicas estão na lista
+    int songCount = 0;
+    // array das musicas
+    String[][] queueArray;
+
     public Player() {
 
+        // botao que adiciona as musicas
         ActionListener btnAddSongOK = e ->{
+            // cria um array temp com uma posicao a mais e copia o antigo
+            String[][] queueTemp = new String[songCount+1][];
+            for (int i = 0; i < songCount;i++){
+                queueTemp[i] = queueArray[i];
+            }
+            // adiciona a nova musica no array
+            queueTemp[songCount] = addSongWindow.getSong();
+            // guarda no queueArray a lista atualizada
+            queueArray = queueTemp;
+            // atualiza a janela com a nova musica
+            window.updateQueueList(queueTemp);
 
+            songID++;
+            songCount++;
         };
 
         ActionListener btnAddSong = e ->{
-            addSongWindow = new AddSongWindow(null, btnAddSongOK, window.getAddSongWindowListener());
+            addSongWindow = new AddSongWindow(Integer.toString(songID), btnAddSongOK, window.getAddSongWindowListener());
         };
 
         window = new PlayerWindow(null, null, btnAddSong,
